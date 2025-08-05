a514b3b9a8a4c90c116b4b99d01fb49aab533d35
abbasdp <abbasdp@proton.me>
2025-08-05 10:55:59 +0330

    forward url mashhadlud >> mashhadlug.sudoshz.ir

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://mashhadlug.org'
+SITEURL = 'https://mashhadlug.sudoshz.ir'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
