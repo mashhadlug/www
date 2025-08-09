53e5a6cc395a6e7eb3ccd603da6c3d3fc63a6078
abbasdp <abbasdp@proton.me>
2025-08-09 13:58:51 +0330

    edit Slug >> mashhadlug.org

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://mashhadlug.sudoshz.ir'
+SITEURL = 'www.mashhadlug.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
